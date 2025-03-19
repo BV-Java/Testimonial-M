35619586f1ab8a91168c8637c94b642c2b55bf0a
BV-Java <balaji27122003s@gmail.com>
2025-03-19 23:26:34 +0530

    App for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ import connectToDatabase from "./Database/MongoDb.js";
 import projectRoutes from "./Routes/project.routes.js";
 import connectCloudinary from "./Database/Cloudinary.js";
 import reviewRoutes from "./Routes/reviews.routes.js";
+import path from "path";
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +23,14 @@ app.use("/api/projects/v1", projectRoutes);
 app.use("/api/reviews/v1", reviewRoutes);
 
 app.use(errorHandler);
+
+if (ENV_VARS.nodeEnv === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server Started at ${port}`);
